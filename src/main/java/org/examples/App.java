8b161cbf650c3ffe9406da619fe88a503e0b0451
Luis <luidoc@gmail.com>
2023-04-29 13:14:18 +0200

    Using jib-maven-plugin

--- src/main/java/org/examples/App.java
+++ src/main/java/org/examples/App.java
@@ -1,10 +1,14 @@
 package org.examples;
 
+import org.apache.qpid.jms.JmsConnectionFactory;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication()
+@RegisterReflectionForBinding({JmsTemplate.class, JmsConnectionFactory.class})
 @EnableJms
 public class App {
     public static void main(String[] args) {
